a81d1151252658593e44eca7ea820df3cacf232e
asnan sultan <asnan.sultan676@gmail.com>
2025-01-01 23:44:48 +0500

    test routing added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express=require(`express`);
 const dbConnect=require("./database/index.js");
 const {PORT}=require("./config/index");
-// const router=require("./routes/index.js");
+const router=require("./routes/index");
 
 const app=express();
 
 
 dbConnect();
-app.get("/",(req,res)=>res.json({"mesg":"Hellwo World!"}));
-app.listen(PORT,console.log(`Server is running on port ${PORT}`));
+// app.get("/",(req,res)=>res.json({"mesg":"Hellwo World!"}));
+app.use(router);
+app.listen(3000,console.log(`Server is running on port ${3000}`));
