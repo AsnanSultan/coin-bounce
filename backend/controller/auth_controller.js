60e3cf7ce06dc42e6a97cec6df5239fef5ffaf45
asnan sultan <asnan.sultan676@gmail.com>
2025-01-02 00:18:14 +0500

    working on authentication

---
+++ backend/controller/auth_controller.js
@@ -0,0 +1,27 @@
+const { model } = require("mongoose")
+const Joi = require("joi");
+
+
+const passwordPattern = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/);
+const authController = {
+    async register(req, res, next) {
+
+        const userRegisterSchema = {
+            username: Joi.string().min(3).max(30).required(),
+            name: Joi.string().max(30).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().pattern(passwordPattern).required(),
+            confirmPassword: Joi.ref("password")
+        }
+
+        const { error } = userRegisterSchema.validate(req.boday);
+
+        if (error) {
+
+        }
+
+    },
+    async login() { }
+}
+
+model.exports = authController;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,13 @@
 const express=require("express");
-
+const authController=require("../controller/auth_controller");
 const router=express.Router();
 
 //testing
 router.get("/test",(req,res)=>res.json({"msg":"Working"}));
 
+
+router.get("/register",authController.register);
+router.get("/login",authController.login);  
 
 
 module.exports=router;
