437ffd8a3f9a2c6b138e22a742e7664cab8886b1
asnan sultan <asnan.sultan676@gmail.com>
2025-01-22 12:50:56 +0500

    Protected routes and error page created

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from "./component/Footer/Footer";
 import Home from "./pages/Home/Home"
 import styles from "./App.module.css";
+import Protected from "./component/Protected/protected";
+import Error from "./pages/Error/Error"
 function App() {
+  const isAuth=false;
   return (
     <div className={styles.container}>
       <BrowserRouter>
@@ -31,17 +34,23 @@ function App() {
               path='/blogs'
               exact
               element={
+                <Protected isAuth={isAuth}>
                 <div className={styles.main}>
                   <div>Blog page</div>
-                </div>}
+                </div>
+                </Protected>
+                }
             />
              <Route
               path='/submit'
               exact
               element={
+                <Protected isAuth={isAuth}>
                 <div className={styles.main}>
                   <div>Submit Blog page</div>
-                </div>}
+                </div>
+                </Protected>
+                }
             />
              <Route
               path='/login'
@@ -57,6 +66,14 @@ function App() {
               element={
                 <div className={styles.main}>
                   <div>Sign in page</div>
+                </div>}
+            />
+             <Route
+              path='*'
+         
+              element={
+                <div className={styles.main}>
+                 <Error/>
                 </div>}
             />
           </Routes>
