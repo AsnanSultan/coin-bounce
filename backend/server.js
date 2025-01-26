f9aeba2349829de122d819dc6232ca4f7271b31c
asnan sultan <asnan.sultan676@gmail.com>
2025-01-26 23:53:12 +0500

    login api integrated in react app

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,21 @@ const { PORT } = require("./config/index");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/errorHandler.js");
 const cookieParser=require("cookie-parser");
-const http=require('http')
+// const http=require('http')
+const cors=require('cors');
+
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+
+}
 
 
 const app = express();
 
 app.use(cookieParser());
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(router);
@@ -21,11 +30,11 @@ app.use(errorHandler);
 app.listen(3000,()=> {console.log(`Server is running on port ${3000}`);
 
 
-var options = {
-    port: 3000,
-    host: '127.0.0.1',
-  };
-var request = http.request(options);
-request.setHeader('content-type', "multipart/form-data");
-request.end();
+// var options = {
+//     port: 3000,
+//     host: '127.0.0.1',
+//   };
+// var request = http.request(options);
+// request.setHeader('content-type', "multipart/form-data");
+// request.end();
 });
--- frontend/my-app/src/pages/Login/Login.jsx
+++ frontend/my-app/src/pages/Login/Login.jsx
@@ -2,8 +2,41 @@ import styles from "./Login.module.css";
 import TextInput from "../../component/TextInput/TextInput";
 import loginSchema from "../../schemas/LoginSchema";
 import { useFormik } from "formik";
+import { login } from "../../api/internal";
+import { setUser } from "../../store/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 
 function Login() {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
+
+    const handleLogin = async () => {
+        const data = {
+            email: values.username,
+            password: values.password
+        };
+        const response = await login(data);
+        if (response.status === 200) {
+            const user = {
+                _id: response.data.user._id,
+                email: response.data.user.email,
+                username: response.data.user.username,
+                auth: response.data.auth
+            }
+            dispatch(setUser(user));
+            navigate("/");
+        }
+        else if (response.code === "ERR_BAD_REQUEST") {
+            // alert("Invalid username or password");
+            setError(response.response.data.message);
+        }
+
+    };
 
     const { values, touched, handleBlur, handleChange, errors } = useFormik({
         initialValues: {
@@ -31,19 +64,20 @@ function Login() {
             />
 
             <TextInput
-             type="password"
-             name="password"
-             values={values.password}
-             onBlur={handleBlur}
-             onChange={handleChange}
-             placeholder="password"
-             error={errors.password && touched.password ? 1 : undefined}
-             errormessage={errors.password}
+                type="password"
+                name="password"
+                values={values.password}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                placeholder="password"
+                error={errors.password && touched.password ? 1 : undefined}
+                errormessage={errors.password}
             />
-            <button className={styles.loginButton}>Log in</button>
+            <button className={styles.loginButton} onClick={handleLogin}>Log in</button>
             <span>
-                Don't have and account? <button className={styles.createAccount}>Register</button>
-            </span>
+                Don't have and account? <button className={styles.createAccount} onClick={() => navigate('/sign-in')}>Register</button>
+            </span>{error!=""?<p className={styles.errorMessage}>{error}</p>:null}
+
         </div>
     );
 
