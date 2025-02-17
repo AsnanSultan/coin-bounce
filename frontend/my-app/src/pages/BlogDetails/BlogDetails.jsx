eea043b62df4f9a1b2eb4679369d7a7fc4a1c237
asnan sultan <asnan.sultan676@gmail.com>
2025-02-17 23:57:27 +0500

    working on Blog Comment

--- frontend/my-app/src/pages/BlogDetails/BlogDetails.jsx
+++ frontend/my-app/src/pages/BlogDetails/BlogDetails.jsx
@@ -12,6 +12,7 @@ const [blog, setBlog]=useState([]);
 const [comments,setComments]=useState([]);
 const [ownsBlog,setOwnsBlog]=useState(false);
 const [newComment,setNewComment]=useState("");
+const [reload,setReload]=useState(false);
 const navigate=useNavigate();
 const param=useParams();
 const blogId=param.id;
@@ -19,7 +20,23 @@ const blogId=param.id;
 const username=useSelector((state)=>state.user.usernamme);
 const userId=useSelector((state)=>state.user._id);
 
+const postCommentHandler=async()=>{
+const data={
+    author:userId,
+    blog:blogId,
+    content:newComment
+}
+const response=await postComment(data);
+if(response.status===201){
+setComments("");
+setReload(!reload)
+}
 
+}
+
+const deleteBlogHandler=async()=>{
+
+}
 
 return (
     <div className={styles.detailsWrapper}>
@@ -33,8 +50,47 @@ return (
             </p>
             </div>
         </div>
+        <div   className={styles.photo}>
+            <img src={blog.photo} width={250} height={250} />
+        </div>
+<p className={styles.content}>
+    {blog.content}
+</p>
+{
+    ownsBlog &&(
+        <div className={styles.control}>
+            <button className={styles.edit}
+            onClick={()=>{}}
+            >
+Edit
+            </button>
+            <button className={styles.delete}
+            onClick={deleteBlogHandler}
+            >
+Delete
+            </button>
+            </div>
+    )
+}
+        <div className={styles.right}>
 
-        <div className={styles.right}></div>
+            <div className={styles.commentsWrapper}>
+                <CommentList/>
+                <div className={styles.postComment}>
+                    <input 
+                    className={styles.input}
+                    placeholder='comments goes here..'
+                    value={newComment}
+                    onChange={(e)=>setNewComment(e.target.value)}
+                    type="text" />
+                    <button className={styles.posthCommentButton}
+                    onClick={postCommentHandler}
+                    >
+Post
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 );
 }
